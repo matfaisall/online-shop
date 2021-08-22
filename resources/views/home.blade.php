34e2e39b7ce22b9f63d33d723b00e622d0b404a1
matfaisal <muh.faisal572@gmail.com>
2021-08-23 02:40:19 +0700

    memasang laravel-livewire

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @livewire('counter')
                 </div>
             </div>
         </div>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h2>Muhammad Faisal</h2>
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h1>{{ $count }}</h1>
+        <button wire:click="decrement">-</button>
+    </div>
+</div>
